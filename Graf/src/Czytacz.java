ae5523fc47bffc88feb0c4c42f2b0f1f978b8ac6
Neuros2048 <95345778+Neuros2048@users.noreply.github.com>
2022-05-14 14:47:36 +0200

    usuwanie komentrzy

--- Graf/src/Czytacz.java
+++ Graf/src/Czytacz.java
@@ -47,36 +47,12 @@ public class Czytacz  {
                     System.out.println("Waga drogi nie możebyć ujemna a ma wartośc "+waga);
                     return;
                 }
-                //System.out.println(polaczenie+"ds"+i);
                 if(!graf.dodaj_droge(i, polaczenie, waga)){
                     System.out.println("Niewłaściwe połączeie wieszchołków");
                     return;
                 }
                 
-            }/*
-            //Linia =skan.nextLine();
-            //polaczenie = Linia.
-            for(j=0;j<4;j++){
-               // if (skan.)
             }
-            liter = 's';
-            if (skan.hasNextInt()){
-                polaczenie = skan.nextInt();
-                if (skan.hasNext()){
-                    System.out.println("dsadasdasda");
-                }
-                c = skan.next();
-                
-                
-                //c = skan.nextLine();
-                
-            }
-            System.out.println(polaczenie);
-            System.out.printf("%s\n",c.charAt(0));
-            System.out.printf("%c\n",liter);
-            waga = Double.parseDouble(c.substring(1));
-            System.out.println(waga);
-            System.out.println(c.substring(1));*/
             
         }
     }
