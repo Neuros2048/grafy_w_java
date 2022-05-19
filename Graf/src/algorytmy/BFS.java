f0b70e8071d2f5a5344ccbdf382d5faff3562b92
Neuros2048 <95345778+Neuros2048@users.noreply.github.com>
2022-05-19 12:10:36 +0200

    BFS

---
+++ Graf/src/algorytmy/BFS.java
@@ -0,0 +1,37 @@
+package algorytmy;
+
+import java.util.ArrayList;
+
+public class BFS extends Algorymt_przechodzenia{
+    private ArrayList<Integer> lista;
+    @Override
+    protected void stworzenie(int poczontek) {
+        lista = new ArrayList<>();
+    }
+
+    @Override
+    protected int nastepny() {
+        int startx = lista.get(0);
+        lista.remove(0);
+        return startx;
+    }
+
+    @Override
+    protected void nowy_wieszcholek(int xdo, int startx) {
+        graf.ustaw_status(xdo, 2);
+        graf.ustaw_wage(xdo, startx, 1);
+        lista.add(xdo);
+        return;
+    }
+
+    @Override
+    protected boolean ponowien_odwiedzic(int xdo, int startx) {
+        return false;
+    }
+
+    @Override
+    protected void ponownie_odwiedony(int xdo, int startx) {
+        return;
+    }
+    
+}
